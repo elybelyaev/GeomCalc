e314f503a05d82d94851ef413ed84615d96ca632
belyaev.ia <belyaev.ia@kontur.ru>
2023-10-27 15:40:09 +0500

    GeomCalc: add calculator class

---
+++ GeometricCalculator/Calculator.cs
@@ -0,0 +1,8 @@
+﻿using GeometricCalculator.Figures;
+
+namespace GeometricCalculator;
+
+public static class Calculator
+{
+    public static double CalculateArea(IWithArea figure) => figure.CalculateArea();
+}
---
+++ GeometricFiguresTests/CalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using GeometricCalculator;
+using GeometricCalculator.Figures;
+
+namespace GeometricFiguresTests;
+
+public class CalculatorTests
+{
+    [Test]
+    public void CalculateArea_ShouldBeSuccessful_OnAnyIWithArea()
+    {
+        var figuresWithArea = new IWithArea[] {new Circle(5), new Triangle(1, 1, 1)};
+
+        foreach (var figure in figuresWithArea)
+        {
+            var areaFunc = () => Calculator.CalculateArea(figure);
+
+            areaFunc.Should().NotThrow();
+        }
+    }
+}
